c1f799e529ea2fbd6bc3e4bb6015105908a0bd11
fladeia <252842.5s@gmail.com>
2021-12-08 21:12:40 -0300

    added hero fix

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -16,20 +16,33 @@ export const Title = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  img {
+    margin-top: 10rem;
+  }
+
+  @media (max-width: 1024px) {
+    img {
+      margin-top: 4rem;
+    }
+  }
 `
 
 export const Text = styled.h1`
   font-family: 'Fraunces', serif;
   font-size: 6rem;
+  font-weight: 900;
   letter-spacing: 0.7rem;
   text-transform: uppercase;
   color: var(--white);
+  margin-top: 6rem;
 
   @media (max-width: 1024px) {
-    font-size: 3rem;
+    font-size: 4rem;
+    margin-top: 4rem;
   }
 
   @media (max-width: 480px) {
-    font-size: 2rem;
+    text-align: center;
   }
 `
